3ce77b235088907ab1053183870ee61a49820ac9
Jtec <jakethomascordon@gmail.com>
2025-09-29 21:09:03 -0400

    Completed Unit Testing for Vehicle CompareTo() method. All checks out

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -58,13 +58,41 @@ public class Vehicle implements Comparable<Vehicle> {
 
     @Override
     public int compareTo(Vehicle vehicleBeingCompared) {
-        int makeComparison = this.make.compareTo(vehicleBeingCompared.make);
-
-        if(makeComparison!=0){
-            return makeComparison;
+        int makeComparison = this.make.name().compareTo(vehicleBeingCompared.make.name());
+        if (makeComparison < 0) {
+            return -1;
+        }
+        if (makeComparison > 0) {
+            return 1;
         }
 
-        return this.getDate().compareTo(vehicleBeingCompared.getDate());
+        // If make is the same, compare by date
+        int dateComparison = this.obtained.compareTo(vehicleBeingCompared.obtained);
+        if (dateComparison < 0){
+            return -1;
+        }
+        if (dateComparison > 0) {
+            return 1;
+        }
+
+        // Otherwise, they are equal
+        return 0;
+    }
+
+    public static void main(String[] args){
+
+        Vehicle match = new Vehicle("589ABC", new Date(2025,7,28),Make.HONDA,9000);
+        Vehicle notMatch = new Vehicle("123XYZ", new Date(2012,8,10),Make.TOYOTA,1000);
+        Vehicle notMatch2 = new Vehicle("987MNO", new Date(2011,11,22),Make.CHEVY,4000);
+        Vehicle match2 = new Vehicle("642XYZ", new Date(2025,7,28),Make.HONDA,9000);
+
+        System.out.println("Test One: " + match.compareTo(notMatch) + " (expected: -1)");
+        System.out.println("Test Two: " + match.compareTo(match2) + " (expected: 0)");
+        System.out.println("Test Three: " + match.compareTo(notMatch2) + " (expected: 1)");
+
+
+
+
     }
 
 }
